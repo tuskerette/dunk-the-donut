2c8fb992192dea31bdaf69e86b0026c5eec32884
tuskerette <valeria.graffeo@gmail.com>
2015-09-30 01:14:35 -0400

    added timer and restart on game over

--- game_window.rb
+++ game_window.rb
@@ -1,6 +1,7 @@
 require 'gosu'
 require_relative 'mug'
 require_relative 'donut'
+require_relative 'timer'
 
 class GameWindow < Gosu::Window
   WIDTH = 640
@@ -16,12 +17,13 @@ class GameWindow < Gosu::Window
     @donuts = []
     @font = Gosu::Font.new(30)
     # @explosion = Gosu::Sample.new("sounds/explosion.wav")
-    # @game_over = false
+    @time = Timer.new(GameWindow)
+    @game_over = false
     @score = 0
   end
 
   def update
-    # if !@game_over
+    if !@game_over
       @mug.update
 
       if rand(100) < 4
@@ -40,14 +42,33 @@ class GameWindow < Gosu::Window
         if donut.off_screen?
           @donuts.delete(donut)
         end
+
+        @time.update
+        # puts @time.seconds
+        if @time.seconds > 3
+          @game_over = true
+        end
       end
-    # end
+    end
+  end
+
+  def restart
+    if Gosu::button_down?(Gosu::KbA)
+      initialize
+    end
   end
 
   def draw
     @background_image.draw(0, 0, 0)
     @mug.draw
+    @font.draw("Time: #{@time.seconds}", WIDTH-120, 0, 1, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
     @font.draw("Score: #{@score}", 0, 0, 1, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
+    if @game_over
+      @font.draw("GAME OVER", WIDTH/3, HEIGHT/2-20, 2, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
+      @font.draw("Press A to start a new game", WIDTH-500, HEIGHT/2+20, 2, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
+      restart
+    end
+
     @donuts.each do|donut|
       donut.draw
     end
